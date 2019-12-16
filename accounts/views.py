f82d1209d2360e32a1d767751187f2f6b19fa996
khaoula-abaidi <abaidik@gmail.com>
2019-12-16 14:11:12 +0100

    filters with django_filters

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
+from .filters import OrderFilter
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -28,7 +29,11 @@ def customer(request, pk_test):
     customer = Customer.objects.get(id = pk_test)
     orders = customer.order_set.all()
     order_count = orders.count()
-    context = {'customer':customer,'orders':orders,'order_count':order_count}
+
+    myfilter =OrderFilter(request.GET, queryset=orders)
+    orders = myfilter.qs
+    context = {'customer':customer,'orders':orders,'order_count':order_count,
+               'myfilter':myfilter}
     return render(request, 'accounts/customer.html',context)
 
 def createOrder(request, pk):
